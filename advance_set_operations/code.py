baabb741a011b6591d1bc794404eb9a776ea0f10
Willmuseve <willmuseve@gmail.com>
2024-01-08 17:54:55 +0300

    2

---
+++ advance_set_operations/code.py
@@ -0,0 +1,9 @@
+#using an example
+
+friends = {"alice", "jennifer", "josette"}
+abroad = {"josette"}
+
+#removes elements from friends that are in abroad
+local = friends.difference(abroad)
+
+print (local)
---
+++ list_tuples_set/code.py
@@ -0,0 +1,20 @@
+#lists 
+#Lists are mutable
+#contents of a list includes strings, integers, floats or doubles
+#lists are enclosed with square brackets
+
+list = ["hello", "world", 1, 20.24]
+
+#tuples
+#are similar to lists except they are enclosed by normal brackets
+# tuples are immutable
+# basic syntax
+
+tuple = ("hello", "world", 1, 20.24)
+
+#sets
+#similar to lists and tuples
+#has curly braces
+#cannot have duplicate elements
+
+set = {"hello", "world"}
